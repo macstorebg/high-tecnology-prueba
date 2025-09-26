743e2775cb62cd2dcf8009b119844446e7c98887
edgar leonardo <macstore27@gmail.com>
2025-09-26 17:02:06 -0500

    navbar

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,15 +1,10 @@
-
-import { DesktopNav } from './DesktopNav';
-import { MobileNav } from './MobileNav';
+import Logo from '../../assets/Logo-blanco.png'
 
 export const Navbar = () => {
 
   return (
-    <div className="relative py-2 px-4 bg-black text-white  z-50">
-       <div className=''>
-          <MobileNav />
-       </div>
-          <DesktopNav />
+    <div className="relative py-2 px-4 flex items-center justify-center bg-transparent z-50">
+       <img src={Logo} alt="" className='size-30 md:size-40'/>
     </div>
   );
 };
